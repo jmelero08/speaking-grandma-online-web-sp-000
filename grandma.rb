c96a34fe15101b9ad6f96505941b59ed7bc41fe1
Jose Melero Martinez <jmelero08@gmail.com>
2020-10-25 20:07:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma (phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase.upcase
+  elsif phrase == phrase.upcase
     return "No, Not since 1938!"
   end
 end
